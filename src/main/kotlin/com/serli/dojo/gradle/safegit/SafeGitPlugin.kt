d85f7fe6a4ab80adb69087539e20530c472e4afb
Quentin PROUST <quentin.proust@serli.com>
2019-02-12 09:38:17 +0100

    do not install git hook on build

--- src/main/kotlin/com/serli/dojo/gradle/safegit/SafeGitPlugin.kt
+++ src/main/kotlin/com/serli/dojo/gradle/safegit/SafeGitPlugin.kt
@@ -3,17 +3,13 @@ package com.serli.dojo.gradle.safegit
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-val taskGroup = "SafeGit Tasks"
+const val taskGroup = "SafeGit Tasks"
 
 class GitHookPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         val installTask = project.tasks.register("installGitHooks", InstallGitHooksTask::class.java) {
             it.group = taskGroup
             it.description = "Install git hook scripts"
-        }
-
-        project.tasks.named("build").configure {
-            it.dependsOn(installTask)
         }
 
         HookNames.names.forEach { hookName ->
